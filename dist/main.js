3aa18bdfea5f8cf6e2339cefcf669d8d697b22b9
eFellow404 <123653222+eFellow404@users.noreply.github.com>
2025-05-24 08:52:09 +0100

    created main page
    app is not functional yet just started

---
+++ dist/main.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/main.ts
+const electron_1 = require("electron");
+function createWindow() {
+    const win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    win.loadFile("index.html");
+}
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on("activate", () => {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
